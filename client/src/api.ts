4cd105f0429ccb460de0626cc9f8983337961b25
Devashish <jadhavom24@gmail.com>
2024-08-01 00:51:25 +0530

    api update

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,2 +1,3 @@
-// export const BASE_URL: string = "https://task-management-crework-assessment.vercel.app/api/v1"
-export const BASE_URL: string = "http://localhost:3001/api/v1";
+export const BASE_URL: string =
+  "https://task-management-crework-assessment.vercel.app/api/v1";
+// export const BASE_URL: string = "http://localhost:3001/api/v1";
