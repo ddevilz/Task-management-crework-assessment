1f54893c4b4cb84c89a4e01b0bc455fe8f3633bd
Devashish <jadhavom24@gmail.com>
2024-07-31 12:34:39 +0530

    build

--- server/dist/app.js
+++ server/dist/app.js
@@ -9,11 +9,17 @@ const config_1 = require("./config/config");
 const cors_1 = __importDefault(require("cors"));
 const index_routes_1 = __importDefault(require("./routes/index.routes"));
 const error_middleware_1 = require("./middleware/error.middleware");
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 (0, db_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_parser_1.default)());
 app.use("/api/v1", index_routes_1.default);
 app.get("/", (_req, res) => {
     res.status(200).send("Hello World!");
