5f1d0cbffed8f81c48973791c39fc4aedb72c29a
Devashish <jadhavom24@gmail.com>
2024-07-31 12:43:03 +0530

    cors fixes

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,14 +9,15 @@ import cookieParser from "cookie-parser";
 const app: Application = express();
 
 connectDb();
-app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:3000"],
+    credentials: true,
     allowedHeaders: ["Authorization", "Content-Type"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
